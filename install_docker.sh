ce8da62a44269c965daffff7935e876ecd3f1308
bbaudry <bbaudry@bbaudry.42.fr>
2022-02-23 13:29:14 +0100

    git init

---
+++ install_docker.sh
@@ -0,0 +1,17 @@
+# install globals tools
+sudo apt update && sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+# add docker's repository
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+# install docker && docker-compose
+sudo apt update && sudo apt upgrade -y && sudo apt install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose && sudo chmod +x /usr/bin/docker-compose
+
+# add docker's right for user
+sudo usermod -aG docker bbaudry
+
+# print docker & docker-compose version for confirm success installation
+docker --version
+docker-compose --version
